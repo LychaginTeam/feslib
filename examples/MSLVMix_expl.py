7de14bbd5b59da5cbb8276a573bb4d329b1ecadf
Alexey <batovalex@yandex.ru>
2025-11-07 19:36:26 +0300

    lib correctness

--- examples/MSLVMix_expl.py
+++ examples/MSLVMix_expl.py
@@ -6,7 +6,7 @@ example methane and ethane
 
 import numpy as np
 import matplotlib.pyplot as plt
-from feslib import MSLVMix
+from feslib.MSLVMix import MSLVMix
 
 
 #%%
@@ -17,7 +17,7 @@ R = 8.314472
 
 #%%
 
-fluid = MSLVMix.MSLVMix()
+fluid = MSLVMix()
 fluid.acPure = np.array([267576.04999955, 634217.7545973])
 fluid.bPure = np.array([29.48676014, 43.21622085])
 fluid.dPure = np.array([35.54066954, 46.1522217])
